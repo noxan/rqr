b5edff496d8f27777be4fab95193bff45a7c5b56
Richard <hello@noxan.net>
2016-07-07 00:34:51 +0200

    mock install command with --save flag

--- rqr/main.py
+++ rqr/main.py
@@ -7,3 +7,8 @@ def cli():
 @cli.command()
 def hello():
     click.echo('Hello rqr!')
+
+@cli.command()
+@click.option('--save', is_flag=True, default=False)
+def install(save):
+    click.echo('install with{0} save'.format('' if save else 'out'))
