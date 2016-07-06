8a4e789a16436b861db3c304baafcb3fae434f36
Richard <hello@noxan.net>
2016-07-07 00:20:45 +0200

    move demo call to demo file and empty module file

---
+++ demo.py
@@ -0,0 +1,3 @@
+from .updater import check_file_for_updates
+
+check_file_for_updates('./requirements/base.txt')
--- rqr/__init__.py
+++
@@ -1,4 +0,0 @@
-from .updater import check_file_for_updates
-
-def main():
-    check_file_for_updates('./requirements/base.txt')
