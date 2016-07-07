df60adc4f3bfac5b5dd8d8b85aa919bc1a753805
Richard <hello@noxan.net>
2016-07-07 11:58:25 +0200

    finish requirements file discovery in folder

--- rqr/migrator.py
+++ rqr/migrator.py
@@ -5,6 +5,8 @@ TARGET_PRODUCTION_PATTERN = '(prod|production)'
 TARGET_DEVELOPMENT_PATTERN = '(dev|development)'
 TARGETS_PATTERN = '(base|{0}|{1})'.format(TARGET_PRODUCTION_PATTERN, TARGET_DEVELOPMENT_PATTERN)
 REQUIREMENTS_REGEX = re.compile('({0}-)?requirements(-{0})?.txt'.format(TARGETS_PATTERN))
+TARGETS_REGEX = re.compile('{0}.txt'.format(TARGETS_PATTERN))
+REQUIREMENTS_FOLDER_NAME = 'requirements'
 
 class Migrator:
     def run(self):
@@ -12,8 +14,10 @@ class Migrator:
 
     def search(self):
         for item in os.listdir(os.getcwd()):
-            if os.path.isdir(item) and item == 'requirements':
-                print('Requirements folder found.')
+            if os.path.isdir(item) and item == REQUIREMENTS_FOLDER_NAME:
+                for subitem in os.listdir(item):
+                    if TARGETS_REGEX.match(subitem):
+                        self.migrate_file(os.path.join(REQUIREMENTS_FOLDER_NAME, subitem))
             elif os.path.isfile(item) and REQUIREMENTS_REGEX.match(item):
                 self.migrate_file(item)
 
